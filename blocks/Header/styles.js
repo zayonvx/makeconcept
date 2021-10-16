f3c38200f330569a7ea97d25455ebf00f3654859
vladislav zaionchkovskiy <zayonvx@gmail.com>
2021-10-16 16:54:28 +0300

    WIP opacity header

--- blocks/Header/styles.js
+++ blocks/Header/styles.js
@@ -6,7 +6,7 @@ import colors from "../../theme/colors";
 const Container = styled(Row)`
     width: 100%;
     background: ${colors.bg.primary};
-    opacity: .8;
+    opacity: .95;
     justify-content: space-between;
     align-items: center;
     padding-left: 25px;
