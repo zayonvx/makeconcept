2b869a5cd779cf6ea58025f04b32548303e70466
vladislav zaionchkovskiy <zayonvx@gmail.com>
2021-10-20 13:59:05 +0300

    fix header 3

--- components/Logo/logo.js
+++ components/Logo/logo.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import {Container} from "./styles";
-import Image from 'next/image'
 
 const Logo = (logo) => {
     return (
         <Container>
-        <Image src={logo.data.url} alt="test" height='95' width='95'/>
+        <img src={logo.data.url} alt="test" height='95' width='95'/>
         </Container>
     );
 };
--- components/MenuContacts/styles.js
+++ components/MenuContacts/styles.js
@@ -1,42 +1,16 @@
 import styled from "styled-components";
 import {textLink} from "../../theme/text";
-import {FaPhone} from 'react-icons/fa';
-import {AiOutlineMail} from 'react-icons/ai';
-
-export const SocialIconMail = styled(AiOutlineMail)`
-  margin-right: auto;
-`
-
-export const SocialIconTel = styled(FaPhone)`
-  margin-right: 3px;
-`
-
-export const ContainerRow = styled.ul`
-  display: flex;
-  flex-direction: row;
-  width: 80px;
-  justify-content: space-between;
-`
 
 export const ContainerColumn = styled.ul`
   display: flex;
   flex-direction: column;
-<<<<<<< HEAD
   &:first-child{
     margin-right: 10px;
-=======
-  margin-bottom: 10px;
-  margin-right: 10px;
-  &:last-child {
-    margin-right: 0;
->>>>>>> 6ad240d467120e834327884b84a4c437d2eefd2b
-  }
 `
 
 export const SocialItem = styled.li`
-<<<<<<< HEAD
-    font-size: 12px;
-    line-height: 12px;
+    font-size: 5px;
+    line-height: 5px;
 `
 
 export const ContactsItem = styled.li`
@@ -44,9 +18,6 @@ export const ContactsItem = styled.li`
     &:last-child {
       margin-bottom: 0;
     }
-=======
-
->>>>>>> 6ad240d467120e834327884b84a4c437d2eefd2b
 `
 
 export const SocialLinkText = styled(textLink)`
@@ -54,10 +25,8 @@ export const SocialLinkText = styled(textLink)`
 
 export const ContactsLinkTel = styled(textLink)`
   font-size: 16px;
-<<<<<<< HEAD
   line-height: 16px;
-=======
->>>>>>> 6ad240d467120e834327884b84a4c437d2eefd2b
+  text-transform: uppercase;
 `
 
 export const ContactsLinkMail = styled(textLink)`
@@ -65,17 +34,6 @@ export const ContactsLinkMail = styled(textLink)`
   line-height: 12px;
 `
 
-export const ContactsWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-`
-
-export const ContactsItem = styled.li`
-  width: 100%;
-  display: flex;
-  justify-content: flex-end;
-`
 export const ContactsWrapper = styled.div`
   display: flex;
   flex-direction: row;
