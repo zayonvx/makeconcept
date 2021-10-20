7a02b245141fc4a053f39317de58e328c1b0dd01
vladislav zaionchkovskiy <zayonvx@gmail.com>
2021-10-20 13:49:11 +0300

    fix header 2

--- components/Logo/logo.js
+++ components/Logo/logo.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Container} from "./styles";
+import Image from 'next/image'
 
 const Logo = (logo) => {
     return (
         <Container>
-        <img src={logo.data.url} alt="test" height='95' width='95'/>
+        <Image src={logo.data.url} alt="test" height='95' width='95'/>
         </Container>
     );
 };
