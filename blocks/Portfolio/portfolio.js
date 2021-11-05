8b7d272f62c3e423aba3e4550fadbb36c956edc9
vladislav zaionchkovskiy <zayonvx@gmail.com>
2021-11-05 16:31:36 +0300

    wip portfolio

--- blocks/Portfolio/portfolio.js
+++ blocks/Portfolio/portfolio.js
@@ -1,17 +1,39 @@
 import React, {useState} from 'react';
 import 'swiper/css/bundle';
-
-
 import {Container} from "../../components/ui/container";
 import {SectionHeader} from "../../theme/text";
+import {Swiper, SwiperSlide} from "swiper/react";
 
+import SwiperCore, {
+    FreeMode,Navigation,Thumbs
+} from 'swiper';
+import {ItemImage} from "../../theme/image";
+import {SwiperWrapper, ThumbsWrapper} from "./styles";
+SwiperCore.use([FreeMode,Navigation,Thumbs]);
 
 const Portfolio = ({data, gallery}) => {
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
     return (
         <Container id="portfolio">
-            <SectionHeader dark>{data.title}</SectionHeader>
-            <div>
-            </div>
+            <SectionHeader dark>Портфолио (на реконструкции)</SectionHeader>
+            <SwiperWrapper>
+                <Swiper style={{'--swiper-navigation-color': '#fff','--swiper-pagination-color': '#fff'}} spaceBetween={10} navigation={true} loop={true} thumbs={{ swiper: thumbsSwiper }} className="mySwiper2">
+                    {gallery.images.map(image =>
+                        <SwiperSlide key={image.url}>
+                            <ItemImage src={image.url}/>
+                        </SwiperSlide>
+                    )}
+                    </Swiper>
+            </SwiperWrapper>
+            <ThumbsWrapper>
+                <Swiper onSwiper={setThumbsSwiper} spaceBetween={10} slidesPerView={4} freeMode={true} watchSlidesProgress={true} loop={true} className="mySwiper">
+                    {gallery.images.map(image =>
+                        <SwiperSlide key={image.url}>
+                            <ItemImage src={image.url}/>
+                        </SwiperSlide>
+                    )}
+                </Swiper>
+            </ThumbsWrapper>
         </Container>
     );
 };
--- blocks/Portfolio/styles.js
+++ blocks/Portfolio/styles.js
@@ -1,1 +1,15 @@
 import styled from "styled-components";
+
+export const SwiperWrapper = styled.div`
+  width: 100%;
+  max-height: 600px;
+  overflow: hidden;
+  margin: 0 auto;
+`
+
+export const ThumbsWrapper = styled.div`
+  width: 100%;
+  max-height: 100px;
+  overflow: hidden;
+  margin: 0 auto;
+`
